b868544cdad61be4a57cb3b07b7c20e2daf41cd1
Naod Wubshet <naodwe7777@gmail.com>
2023-04-10 06:15:53 -0700

    Argc Argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include "main.h"
-int main(int argc, char *argv[])		
+int main(int argc, char *argv[])
 {
-(void) argv; /*Ignore argv*/
-	printf("%d\n", argc - 1);		
-	return (0);		
-}
+(void) argv;
+	printf("%d\n", argc - 1);
+
+	return (0);
